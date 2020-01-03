199a895ceb6aa9290a90b8ef438b57a185c0c615
therecipe <therecipe@users.noreply.github.com>
2020-01-03 04:58:29 +0100

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@ qtdeploy -docker build windows_64_static && rm -rf rcc* moc* vendor && git clean
 cd $OPWD/deploy/windows && zip -q -9 -r ../${NAME}_windows_amd64.zip * && cd $OPWD && rm -rf $OPWD/deploy/windows 
 
 qtdeploy -docker build linux_static && rm -rf rcc* moc* vendor && git clean -f && git reset --hard && docker rmi therecipe/qt:linux_static
-cd $OPWD/deploy/linux && zip -q -9 -r ../${NAME}_linux_amd64.tar.gz * && cd $OPWD && rm -rf $OPWD/deploy/linux
+cd $OPWD/deploy/linux && zip -q -9 -r ../${NAME}_linux_amd64.zip * && cd $OPWD && rm -rf $OPWD/deploy/linux
 
 cd $(go env GOPATH)/src/github.com/therecipe/qt/internal/docker/darwin && ./build_static.sh && cd $OPWD
 qtdeploy -docker build darwin_static && rm -rf rcc* moc* vendor && git clean -f && git reset --hard && docker rmi therecipe/qt:darwin_static
-cd $OPWD/deploy/darwin && zip -q -9 -r ../${NAME}_darwin_amd64.tar.bz2 * && cd $OPWD && rm -rf $OPWD/deploy/darwin
+cd $OPWD/deploy/darwin && zip -q -9 -r ../${NAME}_darwin_amd64.zip * && cd $OPWD && rm -rf $OPWD/deploy/darwin
 
 rm $OPWD/qml/*.qml && qtdeploy -docker -tags=xp build windows_32_shared_xp && rm -rf rcc* moc* vendor && git clean -f && git reset --hard && docker rmi therecipe/qt:windows_32_shared_xp
 cd $OPWD/deploy/windows && zip -q -9 -r ../${NAME}_windows_386_xp.zip * && cd $OPWD && rm -rf $OPWD/deploy/windows 
